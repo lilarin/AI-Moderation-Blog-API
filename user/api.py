16b12020647518de17dd1a63a4344fdca48474fb
lilarin <80650911+lilarin@users.noreply.github.com>
2024-10-23 23:33:44 +0300

    Added endpoint to allow password changing and included user endpoints in the user app

--- user/api.py
+++ user/api.py
@@ -1,8 +1,10 @@
 from ninja_jwt.routers.obtain import obtain_pair_router
 from ninja_jwt.routers.verify import verify_router
 from ninja import NinjaAPI
+from user.views import router as user_router
 
 api = NinjaAPI()
 
 api.add_router("token/", router=obtain_pair_router)
 api.add_router("token/", router=verify_router)
+api.add_router("user/", router=user_router)
--- user/views.py
+++ user/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from ninja.errors import HttpError
 from ninja_jwt.authentication import JWTAuth
 from ninja import Router
+from functools import wraps
 
-from user.schemas import UserSchema
+from user.schemas import (
+    UserSchema,
+    UpdatePasswordSchema
+)
 
 router = Router()
 
@@ -9,3 +16,44 @@ router = Router()
 @router.get("", response=UserSchema, auth=JWTAuth())
 def get_user(request):
     return UserSchema.from_orm(request.user)
+
+
+def password_validation(func):
+    @wraps(func)
+    def wrapper(request, payload, *args, **kwargs):
+        user = request.user
+        new_password = payload.new_password
+        old_password = payload.old_password
+
+        if new_password == old_password:
+            raise HttpError(
+                400,
+                "New password must be different from current password"
+            )
+        if not user.check_password(old_password):
+            raise HttpError(
+                400,
+                "Current password is incorrect."
+            )
+
+        try:
+            validate_password(new_password)
+        except ValidationError as error:
+            raise HttpError(
+                400,
+                " ".join(error.messages)
+            )
+
+        return func(request, payload, *args, **kwargs)
+
+    return wrapper
+
+
+@router.patch("", auth=JWTAuth())
+@password_validation
+def update_profile(request, payload: UpdatePasswordSchema):
+    user = request.user
+
+    user.set_password(payload.new_password)
+    user.save()
+    return {"message": "Password has been successfully updated"}
