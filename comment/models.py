4cae5fe15bfb8ed6d21db9a8a739cb87715a5aa6
lilarin <80650911+lilarin@users.noreply.github.com>
2024-10-24 00:28:24 +0300

    Implemented models for comment and post apps

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from post.models import Post
 
-# Create your models here.
+User = get_user_model()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_blocked = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
