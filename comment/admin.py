726cefa92bb1159099708256060afe89df08270d
lilarin <80650911+lilarin@users.noreply.github.com>
2024-10-24 00:29:13 +0300

    Implemented admin panels for comment and post apps. Also added migrations

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from comment.models import Comment
 
-# Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at', 'is_blocked')
+    search_fields = ('author__username', 'post__title')
+    list_filter = ('is_blocked', 'created_at')
+    ordering = ('-created_at',)
+
+admin.site.register(Comment, CommentAdmin)
--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+# posts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+from comment.models import Comment  # Импортируем модель Comment для использования в админке
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1  # Количество пустых комментариев, которые будут отображаться по умолчанию
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'is_blocked')  # Поля, которые будут отображаться в списке
+    search_fields = ('title', 'author__username', 'text')  # Поиск по заголовку и автору
+    list_filter = ('is_blocked', 'created_at')  # Фильтры
+    inlines = [CommentInline]  # Встраиваем комментарии в админку
+
+    def comments(self, obj):
+        """Свойство для отображения количества комментариев в списке."""
+        return obj.comments.count()
+
+    comments.short_description = 'Comments'  # Название для столбца
+
