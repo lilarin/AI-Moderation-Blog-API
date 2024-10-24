7eacff98021e483ef0dec8e16e7db29f52466ea3
lilarin <80650911+lilarin@users.noreply.github.com>
2024-10-24 11:14:48 +0300

    Merged api to one file to fix error with documentation

--- social_service/api.py
+++ social_service/api.py
@@ -1,10 +1,13 @@
 from ninja_jwt.routers.obtain import obtain_pair_router
 from ninja_jwt.routers.verify import verify_router
 from ninja import NinjaAPI
+
 from user.views import router as user_router
+from post.views import router as post_router
 
 api = NinjaAPI()
 
 api.add_router("token/", router=obtain_pair_router)
 api.add_router("token/", router=verify_router)
 api.add_router("user/", router=user_router)
+api.add_router("posts/", router=post_router)
