19a487b9ebe478a5820362a9012564384fe0f965
lilarin <80650911+lilarin@users.noreply.github.com>
2024-10-24 16:48:26 +0300

    Refactored post views.py to improve codestyle

--- post/views.py
+++ post/views.py
@@ -1,19 +1,27 @@
-from functools import wraps
-
+from django.http import HttpRequest
 from ninja import Router
-from ninja.errors import HttpError
 from ninja.responses import Response
 from ninja_extra import status
 from ninja_jwt.authentication import JWTAuth
-from pydantic import ValidationError
-from post.schemas import PostSchema, CommentSchema, CreatePostSchema
+
+from post.decorators import (
+    post_exist,
+    has_delete_access,
+    has_edit_access
+)
 from post.models import Post
+from post.schemas import (
+    PostSchema,
+    CommentSchema,
+    CreatePostSchema,
+    UpdatePostSchema
+)
 
 router = Router()
 
 
 @router.get("", response=list[PostSchema])
-def get_posts(request):
+def get_posts(request: HttpRequest) -> list[PostSchema]:
     posts = Post.objects.all()
     post_schemas = []
 
@@ -28,44 +36,64 @@ def get_posts(request):
     return post_schemas
 
 
-@router.post("/create", response=PostSchema, auth=JWTAuth())
-def register_user(request, payload: CreatePostSchema):
-    print(request.user.is_authenticated)
-    try:
-        post = Post(
-            author=request.user,
-            title=payload.title,
-            text=payload.text,
-            reply_time=payload.reply_time
-        )
-        post.save()
-        return PostSchema.from_orm(post)
-    except ValidationError as error:
-        raise HttpError(400, " ".join(error.messages))
+@router.post(
+    "/create",
+    response={200: PostSchema, 400: str},
+    auth=JWTAuth()
+)
+def create_post(
+        request: HttpRequest, payload: CreatePostSchema
+) -> PostSchema:
+    post = Post(
+        author=request.user,
+        title=payload.title,
+        text=payload.text,
+        reply_time=payload.reply_time
+    )
+    post.save()
+    return PostSchema.from_orm(post)
 
 
-def has_access(func):
-    @wraps(func)
-    def wrapper(request, post_id, *args, **kwargs):
-        try:
-            post = Post.objects.get(id=post_id)
-        except Post.DoesNotExist:
-            raise HttpError(
-                status.HTTP_404_NOT_FOUND,
-                "Post not found"
-            )
-        if not (request.user.is_staff or post.author == request.user):
-            raise HttpError(
-                status.HTTP_403_FORBIDDEN,
-                "You do not have permission to delete this post")
-
-        return func(request, post_id, *args, **kwargs)
-    return wrapper
+@router.get(
+    "/{post_id}",
+    response={200: PostSchema, 404: str}
+)
+@post_exist
+def get_post(request: HttpRequest, post_id: int) -> PostSchema:
+    post = Post.objects.get(id=post_id)
+    return post
 
 
-@router.delete("/{post_id}", auth=JWTAuth())
-@has_access
-def delete_post(request, post_id: int):
+@router.patch(
+    "/{post_id}",
+    response={200: PostSchema, 403: str, 404: str}, auth=JWTAuth()
+)
+@post_exist
+@has_edit_access
+def edit_post(
+        request: HttpRequest, post_id: int,
+        payload: UpdatePostSchema
+) -> PostSchema:
+    post = Post.objects.get(id=post_id)
+
+    if payload.title is not None:
+        post.title = payload.title
+    if payload.text is not None:
+        post.text = payload.text
+    if payload.reply_time is not None:
+        post.reply_time = payload.reply_time
+
+    post.save()
+    return PostSchema.from_orm(post)
+
+
+@router.delete(
+    "/{post_id}",
+    response={200: str, 403: str, 404: str}, auth=JWTAuth()
+)
+@post_exist
+@has_delete_access
+def delete_post(request: HttpRequest, post_id: int) -> Response:
     post = Post.objects.get(id=post_id)
     post.delete()
     return Response(
